0368f14699929cec0b1043b88b4865ae58663bc0
Tim Shaker <tim@tmshkr.com>
2020-08-18 09:25:52 -0700

    print 8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,27 @@
 
 import sys
 
+# Instruction Set
+# https://github.com/tmshkr/Computer-Architecture/blob/master/LS8-spec.md#instruction-set
+HLT = 0b00000001
+LDI = 0b10000010
+NOP = 0b00000000
+PRN = 0b01000111
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+        # Set up the branch table
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_hlt
+        self.branchtable[LDI] = self.handle_ldi
+        self.branchtable[PRN] = self.handle_prn
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +33,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            0b10000010,  # LDI R0,8
+            0b00000000,  # R0
+            0b00001000,  # the number 8
+            0b01000111,  # PRN R0
+            0b00000000,  # R0
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -61,5 +75,27 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            ir = self.ram_read(self.pc)
+            self.operand_a = self.ram_read(self.pc + 1)
+            self.operand_b = self.ram_read(self.pc + 2)
+
+            self.branchtable[ir]()
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def handle_hlt(self):
+        self.running = False
+
+    def handle_ldi(self):
+        self.reg[self.operand_a] = self.operand_b
+        self.pc += 3
+
+    def handle_prn(self):
+        print(self.reg[self.operand_a])
+        self.pc += 2
